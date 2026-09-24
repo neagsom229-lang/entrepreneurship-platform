e7dffb334ce176d13f5bf98a475e8df284b518b7
chheang samnang <chheangsamnang.wu@gmail.com>
2026-09-24 21:25:04 +0700

    chore: skip eslint during vercel builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false, // keep type checking ON
+  },
+};
 
 export default nextConfig;
